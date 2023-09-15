16ad3128ea87348a6e108489d229ef82f771cd14
SeungHwa Han <starcjs7@gmail.com>
2023-09-15 10:34:19 +0900

    feat: 제외 키워드(-) 기능 구현 #11 (#12)
    - refactoring - handleSearch function

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useRouter } from 'next/navigation'
 import BookList from "./BookList";
 import { getBookList } from "@/api/getBookList";
@@ -8,38 +8,61 @@ import { BookTypes } from "@/types.d";
 
 export default function ListPage() {
     const router = useRouter();
-    
+
     const [word, setWord] = useState<string>("");
     const [pageNumber, setPageNumber] = useState<number>(1);
     const [bookList, setBookList] = useState<BookTypes[]>([]);
-    
-    useEffect(() => {
-        handleScrollDown();
-    }, [pageNumber])
 
     const handleChangeWord = (e: React.ChangeEvent<HTMLInputElement>) => setWord(e.target.value);
-    
-    const handleSearch = async () => {
-        const response = await getBookList(word, pageNumber);
+
+    const handleSearch = async (pageNum: number) => {
+        const removeKeywordIndex = extractRemoveKeywordIndex(word);
+        const apiSearchWord = getSubstringFromIndex(word, removeKeywordIndex);
+
+        const response = await getBookList(apiSearchWord, pageNum);
         const { data } = response;
-        setBookList(data.books)
+        const fetchBookList = data.books;
+
+
+        function filterBooksByKeyword () {
+            // 제외 키워드(-) 없을 시
+            if(removeKeywordIndex < 0){
+                return fetchBookList;
+            }
+
+            const removeKeyword = extractRemoveKeyword(word, removeKeywordIndex + 1)
+            return removeArrayInSpecificKeyword(fetchBookList, removeKeyword);
+        }
+
+        const filteredBookList = filterBooksByKeyword();
+
+        const isScrollDown = pageNum !== 1;
+        setBookList(prevBookList => isScrollDown ? [...prevBookList, ...filteredBookList] : filteredBookList);
     }
 
-    const handleScrollDown = async () => {
-        const response = await getBookList(word, pageNumber);
-        const { data } = response;
-        setBookList(prevBookList => [...prevBookList, ...data.books])
+    const handleSearchButton = () => {
+        setPageNumber(number => {
+            const initPageNumber = 1;
+            handleSearch(initPageNumber);
+            return initPageNumber;
+        });
     }
-    
-    const handleIncreasePageNumber = () => setPageNumber(number => number + 1);
+
+    const handleScrollDown = () => {
+        setPageNumber(number => {
+            const nextPageNumber = number + 1;
+            handleSearch(nextPageNumber);
+            return nextPageNumber;
+        });
+    }
 
     const handleViewDetail = (isbn13?: string) => {
         router.push(`/detail?isbn13=${isbn13}`)
     }
 
     const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === 'Enter') {
-            handleSearch();
+        if (isButtonActive() && e.key === 'Enter') {
+            handleSearchButton();
         }
     };
 
@@ -58,7 +81,7 @@ export default function ListPage() {
                 <button
                     type="button"
                     className={"border rounded bg-white border-black[0.03] px-3 ".concat(isButtonActive() ? "text-black" : "text-gray-200")}
-                    onClick={handleSearch}
+                    onClick={() => handleSearchButton()}
                     disabled={!isButtonActive()}
                 >
                     검색
@@ -68,9 +91,23 @@ export default function ListPage() {
             <BookList
                 bookList={bookList}
                 onClick={handleViewDetail}
-                onChangeScroll={handleIncreasePageNumber}
+                onChangeScroll={handleScrollDown}
             />
 
         </main>
     )
+}
+
+const getSubstringFromIndex = (keyword: string, index: number): string => {
+    return index >= 0 ? keyword.substring(0, index) : keyword
+}
+
+const removeArrayInSpecificKeyword = (arr: BookTypes[], keyword: string) => {
+    return arr.filter(book => book.title.toLowerCase().indexOf(keyword) < 0);
+}
+
+const extractRemoveKeywordIndex = (keyword: string) => keyword.indexOf('-');
+
+const extractRemoveKeyword = (keyword: string, startIndex: number) => {
+    return keyword.substring(startIndex, keyword.length);
 }
